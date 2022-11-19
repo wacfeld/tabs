5ff1f3efc5f24e054dbb79551d75803a9a5e2438
daniel yu <wacfeldwang333@gmail.com>
2022-11-18 20:25:59 -0500

    write and test make_vlq()

--- main.c
+++ main.c
@@ -23,16 +23,45 @@ void set_division(uint d)
   // due to this constraint the highest bit is 0, as required
 
   division[0] = d >> 8; // highest 8 bits
-  division[1] = d; // lowest 8 bits
+  division[1] = d;      // lowest 8 bits
 }
 
 // create variable length quantity from integer
-char *make_vlc(uint n)
+// returns number of bytes (at most 4), writes bytes into vlq
+int make_vlq(uint n, uchar *vlq)
 {
+  // largest division allowed is 7 Fs
+  assert(n <= 0x0FFFFFFF);
   
+  // write reverse order into vlq
+  int len = 0;
+  while(n)
+  {
+    vlq[len] = n & 0x7F; // mask out lowest 7 bits
+    vlq[len] |= 0x80;    // set bit 7
+    
+    len++;
+    n >>= 7;             // shift right 7 bits
+  }
+
+  vlq[0] &= 0x7F; // clear bit 7 of lowest significance byte
+  
+  // reverse bytes
+  for(int i = 0; i < len/2; i++)
+  {
+    uchar temp = vlq[i];
+    vlq[i] = vlq[len-1 - i];
+    vlq[len-1 - i] = temp;
+  }
+
+  return len;
 }
 
 int main()
 {
-  assert(sizeof(int) >= 4);
+  uchar vlq[4] = {0};
+  int x = 0;
+  scanf("%x", &x);
+  int len = make_vlq(x, vlq);
+  printf("%d %02x %02x %02x %02x\n", len, vlq[0], vlq[1], vlq[2], vlq[3]);
 }

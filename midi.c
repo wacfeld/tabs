b9a86dafa292f9b2ce4be6e38176ca887a466563
daniel yu <wacfeldwang333@gmail.com>
2022-11-20 07:06:53 -0500

    remove obsolete midi header code

--- midi.c
+++ midi.c
@@ -11,28 +11,6 @@ struct bytes
   uchar *b;
 };
 
-// initial header chunk; chunk type followed by length (6 bytes)
-const uchar head[] = {'M', 'T', 'h', 'd', 0, 0, 0, 6};
-
-// 16 bit format; 0 means single track
-const uchar format[] = {0, 0};
-
-// 16 bit track count
-const uchar ntrks[] = {0, 1};
-
-// 16 bit division (configurable)
-uchar division[2];
-
-void set_division(uint d)
-{
-  // must fit within (unsigned) 15 bits
-  assert(d <= 32767);
-  // due to this constraint the highest bit is 0, as required
-
-  division[0] = d >> 8; // highest 8 bits
-  division[1] = d;      // lowest 8 bits
-}
-
 struct bytes make_header(uint format, uint tracks, uint division)
 {
   assert(format <= 0xFFFF);

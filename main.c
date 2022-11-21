5e80ae7f7c5638fe0ad0251462a57ecf6e8c0841
daniel yu <wacfeldwang333@gmail.com>
2022-11-21 01:25:39 -0500

    use make_midi_event()

--- main.c
+++ main.c
@@ -13,9 +13,12 @@ int main()
   struct bytes sig = make_timesig(2, 4);
   struct bytes tempo = make_tempo(80, 2, 4);
 
-  struct bytes note = make_bytes(3, 0x99, 0x33, 0x59);
-  struct bytes off = make_bytes(3, 0x89, 0x33, 0x59);
+  // struct bytes note = make_bytes(3, 0x99, 0x33, 0x59);
+  // struct bytes off = make_bytes(3, 0x89, 0x33, 0x59);
   struct bytes end = make_bytes(3, 0xff, 0x2f, 0x00);
+
+  struct bytes note = make_midi_event(NOTE_ON, 9, 2, 0x27, 0x59);
+  struct bytes off = make_midi_event(NOTE_OFF, 9, 2, 0x27, 0x59);
   
   struct bytes evs[] = {make_mtrk_event(0, sig), make_mtrk_event(0, tempo), make_mtrk_event(0, note), make_mtrk_event(0, off), make_mtrk_event(0, end)};
   
